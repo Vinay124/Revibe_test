ba4166ac1dc0588d5402ea412875662c04f7e7d4
Vinay124 <vinaykulkarni404@gmail.com>
2022-08-12 20:02:14 +0530

    main

--- src/Pages/Split_squats.js
+++ src/Pages/Split_squats.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../Styles/main.css'
+import Navbar from '../Components/Navbar'
 
 export default function Split_squats() {
   return (
@@ -8,17 +9,28 @@ export default function Split_squats() {
         <div className='container'>
             <div className='splay-dash'>
                 <div className='splay-img-bx'>
-                    <img src='../assets/img.jpg' alt='/'/>
+                    <img src='../assets/gym8.jpg' alt='/'/>
                 </div>
                 <div className='progressbar'></div>
                     <div className='splay-text-col'>
-                        <div className=''></div>
-                        <div className=''></div>
-                        <div className=''></div>
+                        <div className='sm-text'>
+                            <p>2 of 6</p>
+                            <span>Split Squats - <b> X10</b></span>
+                        </div>
+                        <div className='sm-lg'>
+                            <h1>00:19</h1>
+                        </div>
                     </div>
+                <div className='p-btns'>
+                    <button className='btn-last'><i id="skip" className='bx bx-skip-previous'/></button>
+                    <button className='btn-play'><i id="play" className='bx bx-pause'/></button>
+                    <button className='btn-skip'><i id="next" className='bx bx-skip-next'/></button>
+                </div>
             </div>
         </div>
     </section>
+
+    <Navbar/>
   </>
   )
 }
